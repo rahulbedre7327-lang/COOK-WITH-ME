0c3f0cb91abbfe3df4b3b5b404fcaefb82d0068a
Rahul Bedre <rahulbedre7327@gmail.com>
2026-08-11 01:12:47 +0530

    Updated recipe pages and CSS

--- database/cookwithme.sql
+++ database/cookwithme.sql
@@ -1,6 +1,13 @@
 USE CookWithMe;
 
-SELECT employee_name FROM employees;
-WHERE salary > ANY (
-    SELECT salary FROM employees WHERE department_id=5
-);
+SELECT 
+    r.recipe_id,
+    r.recipe_name,
+    i.name AS ingredient,
+    ri.quantity
+FROM recipes r
+JOIN recipe_ingredients ri
+    ON r.recipe_id = ri.recipe_id
+JOIN ingredients i
+    ON ri.ingredient_id = i.id
+ORDER BY r.recipe_id;
